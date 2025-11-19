23c94c4ccc9696baf62d2c98ead04fdf08a496a0
GitHub Action <action@github.com>
2025-11-19 16:33:47 +0000

    Initialize template repository proper
    Set package name to com.github.evan907cross.firstmcmod
    Set project name to FirstMcMod
    Set modid to firstmcmod

--- src/main/java/com/github/evan907cross/firstmcmod/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/evan907cross/firstmcmod/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.evan907cross.firstmcmod.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/evan907cross/firstmcmod/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/evan907cross/firstmcmod/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.evan907cross.firstmcmod.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
